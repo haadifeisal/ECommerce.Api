4b21601f42b839bfaf6a4380c033804a3e663d01
haadifeisal <haadii_94@hotmail.com>
2022-11-22 15:24:45 +0100

    * Products model.

--- ECommerce.Api/Repositories/ECommerce/Models/Product.cs
+++ ECommerce.Api/Repositories/ECommerce/Models/Product.cs
@@ -2,5 +2,18 @@
 {
     public class Product
     {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public long Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Brand { get; set; }
+
+        public int QuantityInStock { get; set; }
     }
 }
